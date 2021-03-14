086f91fcf3c63a74033925a334a9f2722fea7b72
Josh Padilla <jdp237@nau.edu>
2021-03-14 03:49:42 -0700

    Products, product and navbar components added with appropriate styling

---
+++ e-commerce/src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product"
+
+const products = [
+    { 
+        id: 1,
+        name: "Shoes",
+        description: "Running shoes",
+        price: "$5",
+        image: "https://images.ctfassets.net/od02wyo8cgm5/2xrqztkhkZHj7A0LX2r0aO/a95b3d48192ac06b84edd3ad97fa1a4d/cloudstratus-fw20-mineral_rust-m-g1.png?w=1440&h=1440&fl=progressive&q=61&bg=rgb:f7f7f7&fm=jpg"
+    },
+    {
+        id: 1,
+        name: "Macbook",
+        description: "Apple Macbook",
+        price: "$10",
+        image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000"
+    },
+]
+
+const Products = () => {
+    return (
+        <main>
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    </main>
+    )
+
+}
+
+export default Products;
