3bad93fda8857edfbf1d747e1721244344dd2f49
Josh Padilla <jdp237@nau.edu>
2021-03-20 14:10:58 -0700

    add onClick events for the cart

--- src/App.js
+++ src/App.js
@@ -23,6 +23,24 @@ const App = () => {
         setCart(cart);
     };
 
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+
+        setCart(cart);
+    };
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart);
+    };
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+
+        setCart(cart)
+    };
+
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -39,7 +57,12 @@ const App = () => {
                         <Products products={products} onAddToCart={handleAddToCart} />
                     </Route>
                     <Route exact path="/cart">
-                        <Cart cart={cart} />
+                        <Cart
+                            cart={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
                     </Route>
                 </Switch>
             </div>
